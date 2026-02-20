0426c21df2218cd72cef03657f0c4af865112b1a
Abdalla Nasr <113526886+abdallanasr@users.noreply.github.com>
2026-02-20 02:03:04 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -18,18 +18,18 @@ ticket_state=$(awk -v ticket_no=$ticket_number '{ if ($1 == ticket_no) print $2 
 
 if [ $ticket_state == "closed" ]
 then
-	# Get event number(s)
-	event_number=$(awk -v ticket_no=$ticket_number '{ if ($1 == ticket_no) print $2 }' events_tickets)
-	event_state=$(awk -v event_no=$event_number '{ if ($1 == event_no) print $2 }' events_status)
-	if [ $event_state == "opened" ]
-	then
-		sed -i '/6192fd9a-9b03-71ec-01b9-0a4e23470001/s/opened/closed/' events_status
-		echo "Event [$event_number] has been marked as CLOSED successfully."
-		#echo $event_number closed >> events_status
-	else
-		echo "Event already closed."
-	fi
+        # Get event number(s)
+        event_number=$(awk -v ticket_no=$ticket_number '{ if ($1 == ticket_no) print $2 }' events_tickets)
+        event_state=$(awk -v event_no=$event_number '{ if ($1 == event_no) print $2 }' events_status)
+        if [ $event_state == "opened" ]
+        then
+                sed -i "/$event_number/s/opened/closed/" events_status
+                echo "Event [$event_number] has been marked as CLOSED successfully."
+                #echo $event_number closed >> events_status
+        else
+                echo "Event already closed."
+        fi
 else
-	echo "Incident Status : OPEN"
-	echo "No action required. Event remains OPEN."
-fi  
+        echo "Incident Status : OPEN"
+        echo "No action required. Event remains OPEN."
+fi
